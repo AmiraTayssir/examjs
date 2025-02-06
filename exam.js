c0038cba9950b302e4d02f20b638edec61ad0d9f
AmiraTayssir <tayssiramira55@gmail.com>
2025-02-06 16:15:00 +0100

    Update exam.js

--- exam.js
+++ exam.js
@@ -1,26 +1,35 @@
 (function () {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-  
-    var helloSpeaker = {
-      speak: function (name) {
-        console.log("Hello " + name);
-      }
-    };
-  
-    var byeSpeaker = {
-      speak: function (name) {
-        console.log("Goodbye " + name);
-      }
-    };
-  
-    for (var i = 0; i < names.length; i++) {
-      var firstLetter = names[i].charAt(0).toLowerCase();
-  
-      if (firstLetter === 'j') {
-        byeSpeaker.speak(names[i]);
-      } else {
-        helloSpeaker.speak(names[i]);
-      }
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  var helloSpeaker = {
+    speak: function (name) {
+      return "Hello " + name;
     }
-  })();
-  
+  };
+
+  var byeSpeaker = {
+    speak: function (name) {
+      return "Goodbye " + name;
+    }
+  };
+
+  var outputList = document.getElementById("output");
+
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+    var message;
+
+    if (firstLetter === 'j') {
+      message = byeSpeaker.speak(names[i]);
+    } else {
+      message = helloSpeaker.speak(names[i]);
+    }
+
+    console.log(message);
+
+
+    var listItem = document.createElement("li");
+    listItem.textContent = message;
+    outputList.appendChild(listItem);
+  }
+})();
